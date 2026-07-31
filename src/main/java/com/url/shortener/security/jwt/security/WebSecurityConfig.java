99c03ab0afc48db873fb156eeab0f3d4fdfbf1f6
Swastik Banerjee <swastikb.aot@gmail.com>
2026-07-31 13:51:17 +0530

    cors policy fixing prt4

--- src/main/java/com/url/shortener/security/jwt/security/WebSecurityConfig.java
+++ src/main/java/com/url/shortener/security/jwt/security/WebSecurityConfig.java
@@ -74,10 +74,10 @@ public class WebSecurityConfig {
     public String frontendUrl;
 
     @Bean
-    public WebMvcConfigurer corsConfigurer(){
-        return new WebConfig(){
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry){
+            public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins(frontendUrl)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
