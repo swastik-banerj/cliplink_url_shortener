9c3140fbb86a1166d0f61e344579fc172df34fe3
Swastik Banerjee <swastikb.aot@gmail.com>
2026-07-31 12:39:18 +0530

    cors policy fixing prt1

--- src/main/java/com/url/shortener/security/jwt/security/WebSecurityConfig.java
+++ src/main/java/com/url/shortener/security/jwt/security/WebSecurityConfig.java
@@ -51,7 +51,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/public/register").permitAll()
                         .requestMatchers("/{shortUrl}").permitAll()
                         .requestMatchers("/api/urls/**").authenticated()
                         .requestMatchers("/swagger-ui.html", "/v3/api-docs/**", "/swagger-ui/**").permitAll()
