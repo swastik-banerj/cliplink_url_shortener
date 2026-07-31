0ad5637a02af24787d692c5aedeb6f22d31246cd
Swastik Banerjee <swastikb.aot@gmail.com>
2026-07-31 12:53:02 +0530

    cors policy fixing prt2

--- src/main/java/com/url/shortener/security/jwt/security/WebConfig.java
+++ src/main/java/com/url/shortener/security/jwt/security/WebConfig.java
@@ -19,10 +19,10 @@ public class WebConfig implements WebMvcConfigurer {
             public void addCorsMappings(CorsRegistry registry){
                 registry.addMapping("/**")
                         .allowedOrigins(frontendUrl)
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true)
-                        .maxAge(3600);
+                        .maxAge(36000);
             }
         };
     }
--- src/main/java/com/url/shortener/security/jwt/security/WebSecurityConfig.java
+++ src/main/java/com/url/shortener/security/jwt/security/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -50,6 +51,7 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/public/register").permitAll()
                         .requestMatchers("/{shortUrl}").permitAll()
